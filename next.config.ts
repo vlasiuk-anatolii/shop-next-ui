072884be1ead50f840b65fa48d551657801d03f5
Vlasiuk Anatolii <anatolii.vlasiuk@bndigital.co>
2025-04-09 22:16:05 +0300

    feat: was added displaing images

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "localhost",
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: 'shop-backend-env.eba-kpsmmad2.eu-north-1.elasticbeanstalk.com',
       }
     ]
   }
